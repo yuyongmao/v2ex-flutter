9aa8df8249cc9bef06a077de61c3817637f81593
yuyongmao <yuyongmao@126.com>
2018-03-12 23:16:07 +0800

    drawer界面绘制...

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
 
 void main() => runApp(new MyApp());
 
@@ -43,16 +44,66 @@ class _MyHomePageState extends State<MyHomePage> {
       drawer: new Drawer(
         child: new Column(
           children: <Widget>[
-            new UserAccountsDrawerHeader(
-              accountName: const Text('V2EXER'),
-              accountEmail: const Text('v2exer@v2ex.com'),
-              currentAccountPicture: const CircleAvatar(
-                backgroundImage:const NetworkImage('https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=962905290,1771717332&fm=27&gp=0.jpg'),
+            new DrawerHeader(
+              child: new Stack(
+                fit: StackFit.expand,
+                children: <Widget>[
+                  new Image.asset('images/account_bg.jpg',
+                      fit: BoxFit.fill, height: 400.0, width: 600.0),
+                  new BackdropFilter(
+                    filter: new ui.ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+                    child: new Container(
+                      color: Colors.black.withOpacity(0.5),
+                      child: new Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          const Padding(
+                              padding: const EdgeInsets.only(top: 20.0)),
+                          const CircleAvatar(
+                            backgroundImage: const NetworkImage(
+                                'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1125401688,1369076827&fm=27&gp=0.jpg'),
+                            radius: 32.0,
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
+              margin: new EdgeInsets.all(0.0),
+              padding: new EdgeInsets.all(0.0),
             ),
             new ListTile(
-              leading: new Icon(Icons.change_history),
-              title: new Text('Change history'),
+              leading: new Icon(Icons.cached),
+              title: new Text('最新'),
+              onTap: () {
+                Navigator.pop(context); // close the drawer
+              },
+            ),
+            new ListTile(
+              leading: new Icon(Icons.adjust),
+              title: new Text('节点'),
+              onTap: () {
+                Navigator.pop(context); // close the drawer
+              },
+            ),
+            new ListTile(
+              leading: new Icon(Icons.favorite),
+              title: new Text('收藏'),
+              onTap: () {
+                Navigator.pop(context); // close the drawer
+              },
+            ),
+            new ListTile(
+              leading: new Icon(Icons.access_alarm),
+              title: new Text('提醒'),
+              onTap: () {
+                Navigator.pop(context); // close the drawer
+              },
+            ),
+            new ListTile(
+              leading: new Icon(Icons.account_circle),
+              title: new Text('个人'),
               onTap: () {
                 Navigator.pop(context); // close the drawer
               },
